060b79df7aee41acad696f8ee47c2a7e4c938c2a
mahmoudbelalcs@gmail.com <mahmoudbelal@FCI012>
2021-10-03 05:25:12 +0200

    - apply firebase remote config with 3 params 1- checking the application latest version to ask the user to update the application from google play 2- text of the button will updated dynamically 3- number of paging [size of the loaded list ] will be only setted dynamically waiting the user click action - update some ux messages

--- app/src/main/java/com/mahmoud/belal/postsapp/utils/Utils.kt
+++ app/src/main/java/com/mahmoud/belal/postsapp/utils/Utils.kt
@@ -1,10 +1,20 @@
 package com.mahmoud.belal.postsapp.utils
 
 import android.content.Context
+import android.content.Intent
+import android.net.Uri
 import android.view.View
 import android.widget.Toast
 import androidx.appcompat.app.AlertDialog
 import androidx.fragment.app.Fragment
+import com.mahmoud.belal.postsapp.BuildConfig
+import com.mahmoud.belal.postsapp.R
+import android.content.pm.PackageManager
+
+import android.content.ComponentName
+
+
+
 
 fun View.show() {
     visibility = View.VISIBLE
@@ -42,9 +52,73 @@ fun Fragment.showAlert(title: String, messageDetails: String) {
     val builder = AlertDialog.Builder(requireContext())
     builder.setTitle(title)
     builder.setMessage(messageDetails)
-    builder.setPositiveButton(android.R.string.cancel) { dialog, which ->
+    builder.setPositiveButton(R.string.cancel) { dialog, which ->
         dialog.dismiss()
     }
     builder.create()
     builder.show()
+}
+
+fun Fragment.showUpdateApplicationAlert(title: String, messageDetails: String) {
+    val APP_LINK_ON_GOOGLE_PLAY =
+        "play.google.com/store/apps/details?id=" + BuildConfig.APPLICATION_ID
+
+    val builder = AlertDialog.Builder(requireContext())
+    builder.setTitle(title)
+    builder.setMessage(messageDetails)
+    builder.setNegativeButton(R.string.cancel) { dialog, which ->
+        dialog.dismiss()
+    }
+    builder.setPositiveButton(R.string.update) { dialog, which ->
+        var intent = Intent(Intent.ACTION_VIEW, Uri.parse("market://" + APP_LINK_ON_GOOGLE_PLAY))
+        try {
+            startActivity(intent)
+        } catch (exception: Exception) {
+            intent = Intent(Intent.ACTION_VIEW, Uri.parse("https://" + APP_LINK_ON_GOOGLE_PLAY))
+            startActivity(intent)
+        }
+    }
+
+    builder.create()
+    builder.show()
+}
+
+
+fun changeAppIconDynamically(context: Context, isNewIcon: Boolean) {
+    val pm = context.applicationContext.packageManager
+    if (isNewIcon) {
+        pm.setComponentEnabledSetting(
+            ComponentName(
+                context,
+                "com.mahmoud.belal.postsapp.application.MainActivity1"
+            ),  //com.example.dummy will be your package
+            PackageManager.COMPONENT_ENABLED_STATE_DISABLED,
+            PackageManager.DONT_KILL_APP
+        )
+        pm.setComponentEnabledSetting(
+            ComponentName(
+                context,
+                "com.mahmoud.belal.postsapp.application.MainActivity"
+            ),
+            PackageManager.COMPONENT_ENABLED_STATE_ENABLED,
+            PackageManager.DONT_KILL_APP
+        )
+    } /*else {
+        pm.setComponentEnabledSetting(
+            ComponentName(
+                context,
+                "com.mahmoud.belal.postsapp.application.MainActivity1"
+            ),
+            PackageManager.COMPONENT_ENABLED_STATE_ENABLED,
+            PackageManager.DONT_KILL_APP
+        )
+        pm.setComponentEnabledSetting(
+            ComponentName(
+                context,
+                "com.mahmoud.belal.postsapp.application.MainActivity"
+            ),
+            PackageManager.COMPONENT_ENABLED_STATE_DISABLED,
+            PackageManager.DONT_KILL_APP
+        )
+    }*/
 }
